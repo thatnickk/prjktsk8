93392d3e3f6a4b873031c9faf638c210093c391c
thatnickk <62030882+thatnickk@users.noreply.github.com>
2021-03-24 00:43:28 +0200

    added layouts: shopping cart; improved bits

---
+++ projectsk8/scripts/nav-menu-show.js
@@ -0,0 +1,9 @@
+function navShow(){
+    var nav = document.getElementById("navi");
+    if (nav.style.display === "none"){
+        nav.style.display = "inline-block";
+    }
+    else{
+        nav.style.display = "none";
+    }
+}
